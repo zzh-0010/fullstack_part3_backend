0b048eb002b315946f249c8746afb8217c8e3561
zzh-0010 <zhgo0128@outlook.com>
2024-08-04 16:41:13 +0800

    uibuild

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('content', (request) => {
     const body = request.body
