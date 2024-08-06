9fb4ca3344cdf50d02351a7120e4569cb8fa614e
zzh-0010 <zhgo0128@outlook.com>
2024-08-06 22:23:31 +0800

    backend

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+        'error', 'always'
+    ],
+    'arrow-spacing': [
+        'error', { 'before': true, 'after': true }
+    ],
+    'no-console': 0
+    },
+    'plugins': [
+        '@stylistic/js'
+    ],
+    'extends': 'eslint:recommended',
+    'rules': {
+        '@stylistic/js/indent': [
+            'error',
+            2
+        ],
+        '@stylistic/js/linebreak-style': [
+            'error',
+            'unix'
+        ],
+        '@stylistic/js/quotes': [
+            'error',
+            'single'
+        ],
+        '@stylistic/js/semi': [
+            'error',
+            'never'
+        ],
+    }
+  };
+  
