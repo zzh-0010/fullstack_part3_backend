c2f9b381b65325eb4d24fc8f3b21ab209c184ca0
zzh-0010 <zhgo0128@outlook.com>
2024-08-06 21:10:31 +0800

    uibuild

--- models/person.js
+++ models/person.js
@@ -20,7 +20,16 @@ const personSchema = new mongoose.Schema({
         minLength: 3,
         required: true
     },
-    number: String,
+    number: {
+        type: String,
+        validate: {
+            validator: v => {
+                return /^\d{2,3}-\d{6,}$/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number`
+        },
+        required: true
+    },
   })
 
 personSchema.set('toJSON', {
